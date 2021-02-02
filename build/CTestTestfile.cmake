28ab8fe78d672f57d0c1ca781115242f47d6491b
teamo1996 <18810521109@163.com>
2021-02-02 03:25:59 -0800

    隐藏不需要提交的文件

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/teamo/lidar-slam/src/lidar-slam
-# Build directory: /home/teamo/lidar-slam/src/lidar-slam/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
