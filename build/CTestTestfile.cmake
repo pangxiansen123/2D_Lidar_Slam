8227c63f68ef0e8307f8d22695c6fb7a23551735
teamo1996 <18810521109@163.com>
2021-02-02 02:41:35 -0800

    master

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/teamo/lidar-slam/src/lidar-slam
+# Build directory: /home/teamo/lidar-slam/src/lidar-slam/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/teamo/lidar-slam/src/lidar-slam/build/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/teamo/lidar-slam/src/lidar-slam/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/teamo/lidar-slam/src/lidar-slam/build/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/teamo/lidar-slam/src/lidar-slam:$ROS_PACKAGE_PATH"
